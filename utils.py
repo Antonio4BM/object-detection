b090249a44da95c61da40521522d24e079c5f453
Antonio Briseño <red2013@hotmail.com>
2026-03-02 14:23:54 -0600

    feat: add get_bboxes from training loader

--- utils.py
+++ utils.py
@@ -1,6 +1,7 @@
 import torch
 from torch import Tensor
 from collections import Counter
+from torch.utils.data import DataLoader
 
 
 def intersection_over_union(
@@ -148,5 +149,59 @@ def mean_average_precision(
         average_precision.append(torch.trapz(precisions, recalls)) # calcualte are under curve
 
         return sum(average_precisions) / len(average_precisions)
+
+
+def get_bboxes(
+    loader: DataLoader,
+    model: object,
+    iou_threshold: float,
+    threshold: float,
+    pred_format:str ="cells",
+    box_format:str ="midpoint",
+    device:str ="cuda",
+):
+    all_pred_boxes = []
+    all_true_boxes = []
+
+    # make sure model is in eval before get bboxes
+    model.eval()
+    train_idx = 0
+
+    for batch_idx, (x, labels) in enumerate(loader):
+        x = x.to(device)
+        labels = labels.to(device)
+
+        with torch.no_grad():
+            predictions = model(x)
+
+        batch_size = x.shape[0]
+        true_bboxes = cellboxes_to_boxes(labels)
+        bboxes = cellboxes_to_boxes(predictions)
+
+        for idx in range(batch_size):
+            nms_boxes = non_max_suppression(
+                bboxes[idx],
+                iou_threshold=iou_threshold,
+                threshold=threshold,
+                box_format=box_format,
+            )
+
+
+            #if batch_idx == 0 and idx == 0:
+            #    plot_image(x[idx].permute(1,2,0).to("cpu"), nms_boxes)
+            #    print(nms_boxes)
+
+            for nms_box in nms_boxes:
+                all_pred_boxes.append([train_idx] + nms_box)
+
+            for box in true_bboxes[idx]:
+                # many will get converted to 0 pred
+                if box[1] > threshold:
+                    all_true_boxes.append([train_idx] + box)
+
+            train_idx += 1
+
+    model.train()
+    return all_pred_boxes, all_true_boxes
             
     
